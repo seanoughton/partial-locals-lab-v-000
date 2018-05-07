68c2b8483f962b9a18d709f7293ada12ce59676a
Sean Oughton <seanoughton@gmail.com>
2018-05-07 18:37:27 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,8 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(student_name)
-    binding.pry
+    Student.all if student_name.empty?
+
   end
 
 end
